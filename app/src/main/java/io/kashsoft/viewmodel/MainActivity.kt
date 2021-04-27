b2b7495d94a17fa9135747aa950f8cb09986490e
Hammad Yousuf <hammadbhat126@gmail.com>
2021-04-27 07:05:25 +0400

    Add ViewModelFactory
    Add ViewModelFactory to pass data

--- app/src/main/java/io/kashsoft/viewmodel/MainActivity.kt
+++ app/src/main/java/io/kashsoft/viewmodel/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(10)).get(MainViewModel::class.java)
         txtCounter=findViewById(R.id.txtcounter)
         setText()
     }
--- app/src/main/java/io/kashsoft/viewmodel/MainViewModel.kt
+++ app/src/main/java/io/kashsoft/viewmodel/MainViewModel.kt
@@ -3,9 +3,9 @@ package io.kashsoft.viewmodel
 import android.view.View
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel(){
+class MainViewModel (val intialValue:Int): ViewModel(){
 
-    var count:Int = 0
+    var count:Int = intialValue
 
     fun increment() {
         count++
