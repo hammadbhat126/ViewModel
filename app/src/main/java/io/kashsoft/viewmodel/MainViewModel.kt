c6b61f4d4dbda981a96448a2135b6f0e16626f84
Hammad Yousuf <hammadbhat126@gmail.com>
2021-04-27 06:48:30 +0400

    Initial commit

---
+++ app/src/main/java/io/kashsoft/viewmodel/MainViewModel.kt
@@ -0,0 +1,14 @@
+package io.kashsoft.viewmodel
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+
+    var count:Int = 0
+
+    fun increment() {
+        count++
+
+    }
+}
